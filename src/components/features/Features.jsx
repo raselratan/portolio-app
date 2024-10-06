ebf420d6dc88494f212ee5e4c7504b65ae6f8ca6
Rasel Mahmud Ratan <rasel.m.ratan@gmail.com>
2024-10-07 03:06:41 +0600

    features added

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,7 +1,48 @@
+import Title from "../layouts/Title";
+import Card from "./Card";
+
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe} from "react-icons/fa";
+import { SiProgress, SiAntdesign } from "react-icons/si";
+
 const Features = () => {
     return (
-        <section id='features' className="w-full h-[1800px] pt-10 pb-20 flex border-b-[1px] border-b-black">
-            Features
+        <section
+            id='features'
+            className="w-full pb-20 py-20 border-b-[1px] border-b-black"
+        >
+            <Title title="Features" description="What I do" />
+            <div className="grid grid-cols-3 gap-20">
+                <Card
+                    title="Business Stratagy"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+                />
+                <Card
+                    title="App Development"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+                    icon= {<AiFillAppstore />}
+                />
+                <Card
+                    title="SEO Optimisation"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+                    icon= {<SiProgress />}
+                />
+                <Card
+                    title="Mobile Development"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+                    icon= {<FaMobile />}
+                />
+                <Card
+                    title="UX Design"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+                    icon= {<SiAntdesign />}
+                />
+                <Card
+                    title="Hosting Websites"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad"
+                    icon= {<FaGlobe />}
+                />
+            </div>
         </section>
     );
 }
